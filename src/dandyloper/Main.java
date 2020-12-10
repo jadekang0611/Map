39fdb2e06fcb81bf2a7f282fbd26f9fda59f5026
jadekang <jkang.se@gmail.com>
2020-12-10 03:24:10 -0800

    Learned how to create constants for keys.

--- src/dandyloper/Main.java
+++ src/dandyloper/Main.java
@@ -4,17 +4,21 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
+    // Declear constants for keys:
+    public static final String BREAKFAST = "breakfast";
+    public static final String LUNCH = "lunch";
+    public static final String DINNER = "dinner";
 
     public static void main(String[] args) {
         Map<String, String> meals = new HashMap<>();
-        meals.put("breakfast", "waffles");
-        meals.put("lunch", "chicken salad");
-        meals.put("dinner", "bossam");
+        meals.put(BREAKFAST, "waffles");
+        meals.put(LUNCH, "chicken salad");
+        meals.put(DINNER, "bossam");
 
         System.out.println(meals);
 
-        String lunch = meals.remove("lunch");
-        boolean hasLunch = meals.containsKey("lunch");
+        String lunch = meals.remove(LUNCH);
+        boolean hasLunch = meals.containsKey(LUNCH);
         boolean hasChickenSalad = meals.containsValue("chicken salad");
         int size = meals.size();
 
